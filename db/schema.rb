26912d4f2a3cedf77c098443674b77196fdd9712
TATSUYA <tatsuya415077@gmail.com>
2017-01-26 14:25:24 +0900

    migrate files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123043710) do
+ActiveRecord::Schema.define(version: 20170126042839) do
+
+  create_table "chat_group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "chat_group_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["chat_group_id"], name: "index_chat_group_users_on_chat_group_id", using: :btree
+    t.index ["user_id"], name: "index_chat_group_users_on_user_id", using: :btree
+  end
+
+  create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "team"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535
@@ -31,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170123043710) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string   "name",                                null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
