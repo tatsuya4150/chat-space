6d54d9650658d9902d8870c4e25196c2dcd51cd4
TATSUYA <tatsuya415077@gmail.com>
2017-01-26 14:23:00 +0900

    update association in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #association
+  has_many :chat_group_users
+  has_many :users, through: :chat_group_users
+  has_many :messages
 end
