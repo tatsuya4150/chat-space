b118fa11d1e234d21b1984b1fa1b7fb7e87e81fd
TATSUYA <tatsuya415077@gmail.com>
2017-01-28 15:42:34 +0900

    add index_action, new_action, edit_action, create_action to chat_groups_controller and update chat_group.rb

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,7 +1,36 @@
 class ChatGroupsController < ApplicationController
+
+  def index
+    @chat_groups = current_user.chat_groups
+  end
+
   def new
+    @chat_group = ChatGroup.new
+    @chat_group.users << current_user
   end
 
   def edit
   end
+
+  def create
+    @chat_group = ChatGroup.new(chat_group_params)
+    if @chat_group.save
+      redirect_to chat_group_messages_path(@chat_group), notice: "グループが作成されました。"
+    else
+      flash.now[:alert] = "作成に失敗しました"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+    def set_chat_group
+      @chat_group = ChatGroup.find(params[:id])
+    end
+
+    def chat_group_params
+      params.require(:chat_group).permit(:team, user_ids: [])
+    end
 end
--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -3,4 +3,8 @@ class ChatGroup < ApplicationRecord
   has_many :users, through: :chat_group_users
   has_many :messages
 
+  accepts_nested_attributes_for :users, allow_destroy: true
+
+  validates :team, presence: true
+  validates :users, presence: true
 end
