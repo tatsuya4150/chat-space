4c460ebc34e6536967f267b8c9f7e28081b704de
TATSUYA <tatsuya415077@gmail.com>
2017-01-28 15:39:56 +0900

    change routing to nest messages in chat_groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :messages
-  resources :chat_groups
+  root 'chat_groups#index'
+  resources :chat_groups, only: [:index, :new, :edit, :create, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
