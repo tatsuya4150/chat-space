a40549de6c6648e90a6114396b28edb7da9a7439
TATSUYA <tatsuya415077@gmail.com>
2017-01-29 20:40:06 +0900

    clean routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chat_groups#index'
-  resources :chat_groups, only: [:index, :new, :edit, :create, :update] do
+  resources :chat_groups, except: [:show, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
