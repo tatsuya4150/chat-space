dc4f2f561f09cd73d02a245f114b44e660f66dac
TATSUYA <tatsuya415077@gmail.com>
2017-02-03 14:37:51 +0900

    add database-cleaner and rails-i18n

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,4 +50,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, :type => :controller
 end
